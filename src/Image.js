3f9a5f313036c0329425f33c0648f65e9f8b9787
rameez <rameezkhansadozai@gmail.com>
2021-03-23 01:42:54 +0500

    project completed

---
+++ src/Image.js
@@ -0,0 +1,68 @@
+import React from "react";
+
+
+import { Carousel } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+function Image(){
+return(
+
+<div>
+                <div className='container-sm|md|lg|xl' >
+                    <div className="row">
+                        
+                    </div>
+                    <div className="row">
+                        <div className="col-12">
+                            <Carousel>
+
+                                <Carousel.Item>
+                                    <img
+                                        className="d-flex align-items-center justify-content-center min-vh-100 min-vw-100"
+                                        src="https://picsum.photos/500/300?img=1"
+                                        alt="First slide"
+                                    />
+                                    <Carousel.Caption>
+                                        <h3>First slide label</h3>
+                                        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                                    </Carousel.Caption>
+                                </Carousel.Item>
+
+                                <Carousel.Item>
+                                    <img
+                                        className="d-flex align-items-center justify-content-center min-vh-100 min-vw-100"
+                                        src="https://picsum.photos/500/300?img=2"
+                                        alt="Second slide"
+                                    />
+
+                                    <Carousel.Caption>
+                                        <h3>Second slide label</h3>
+                                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                                    </Carousel.Caption>
+                                </Carousel.Item>
+
+                                <Carousel.Item>
+                                    <img
+                                        className="d-flex align-items-center justify-content-center min-vh-100 min-vw-100"
+                                        src="https://picsum.photos/500/300?img=2"
+                                        alt="Third slide"
+                                    />
+                                    <Carousel.Caption>
+                                        <h3>Third slide label</h3>
+                                        <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                                    </Carousel.Caption>
+                                </Carousel.Item>
+
+                            </Carousel>
+                        </div>
+                    </div>
+                </div>
+            </div>)
+            
+        
+        
+        
+        }
+
+ export default Image;       
